ec5d95c1a9ff99b17c9d2194ecd023cea57de90b
ramyaps94 <ramya.sudarshan1994@gmail.com>
2015-07-28 00:23:56 +0530

    ramya |002| tokenizes the string input into integer array list for plateau boundaries

---
+++ src/Tokenizer.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Tokenizer {
+    private String input;
+    private int x;
+    private int y;
+    private int[] tokenizedInput = {};
+
+    public Tokenizer(String input) {
+        this.input = input;
+    }
+
+    public ArrayList<Integer> tokenizeString() {
+        ArrayList<Integer> tokenizedInput = new ArrayList<Integer>();
+        String[] thisInput = this.input.split(" ");
+        x = Integer.parseInt(thisInput[0]);
+        y = Integer.parseInt(thisInput[1]);
+        tokenizedInput.add(x);
+        tokenizedInput.add(y);
+
+        return tokenizedInput;
+    }
+}
